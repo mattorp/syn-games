ba8436317565ce9170f054617ec7df3ea718760d
Mathias Torp <mat.torp@gmail.com>
2021-02-27 22:16:45 +0100

    print and return values

--- midi_server.py
+++ midi_server.py
@@ -13,5 +13,7 @@ def result():
     note = int(request.form['note'])
     velocity = int(request.form['velocity'])
     send_midi((0xb0, note, velocity))
+    print(note, velocity)
+    return '{},{}'.format(note, velocity)
 
 app.run(host='0.0.0.0', port='5002')
