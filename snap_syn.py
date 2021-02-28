584f766f1c73f31b3459afad40c4c7776371f1d4
Mathias Torp <mat.torp@gmail.com>
2021-02-28 21:17:16 +0100

    update uuid and add todo

--- snap_syn.py
+++ snap_syn.py
@@ -1,12 +1,12 @@
 import os
 from time import sleep
-from subprocess import call
 
 SLEEP = 10
 
 while True:
+  # TODO: change to exceute action
   os.system(
-      'open btt://execute_assigned_actions_for_trigger/?uuid=BCB15335-F58B-4BE2-9C54-52877F654953')
+      'open btt://execute_assigned_actions_for_trigger/?uuid=12D19A27-4D74-4D7F-91BA-49B57B977828')
   sleep(2)
   os.system(
       'python3 ./get_pixels.py')
